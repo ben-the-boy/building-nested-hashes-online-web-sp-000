4bf4e62c23a6e9333be486584a6ad6ce8de937cb
Ben Hayes <b.t.hayes@comcast.net>
2020-12-18 17:39:00 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,10 +1,3 @@
 def first_challenge
-   epic_tragedy = {},
-     :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
-     }
-   }
+
 end
